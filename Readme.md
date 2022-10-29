c567000a1c5d160d4667235874d848187f555067
tvk2772 <tvk2772@gmail.com>
2022-10-29 17:34:12 +0300

    firts commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+## Учебный проект по очистке данных
+##### выполнил: *Кулакова Татьяна*
+
+### Размещение файлов данных
++ [*Файл с данными соискателей*](https://drive.google.com/file/d/15PscI9Dj9LjO5Ndq6hMmqXha7robWobh/view?usp=sharing)
++ [*Файл курсов валют*](https://drive.google.com/file/d/1J3rzDi9GE8fZFcN2cI5osbNvmQd8lZeJ/view?usp=sharing)
+### Диаграммы, созданные при помощи библиотеки Plotly
++ [*Гистограмма распределения опыта работы*](diagrams/experience_hist.html)
++[*Коробчатая диаграмма распределения опыта работы*](diagrams/experience_box.html)
---
+++ outliers.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+
+# Quartile Method
+
+def outliers_iqr(data, feature, log_scale=False, left=1.5, right=1.5):
+    if log_scale:
+        x=np.log(data[feature])
+    else:
+        x = data[feature]
+    quartile_1, quartile_3 = x.quantile(0.25), x.quantile(0.75),
+    iqr = quartile_3 - quartile_1
+    lower_bound = quartile_1 - (iqr * left)
+    upper_bound = quartile_3 + (iqr * right)
+    outliers = data[(x<lower_bound) | (x > upper_bound)]
+    cleaned = data[(x>lower_bound) & (x < upper_bound)]
+    return outliers, cleaned
+
+# Z-score method
+
+def outliers_z_score(data, feature, log_scale=False, left=3, right=3):
+    if log_scale:
+        x = np.log(data[feature]+1)
+    else:
+        x = data[feature]
+    mu = x.mean()
+    sigma = x.std()
+    lower_bound = mu - left * sigma
+    upper_bound = mu + right * sigma
+    outliers = data[(x < lower_bound) | (x > upper_bound)]
+    cleaned = data[(x > lower_bound) & (x < upper_bound)]
+    return outliers, cleaned
